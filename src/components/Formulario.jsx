121dbde9a524dcb479975ee10c726f458338842e
Rodrigo <rodrigo.gallardo.6012@gmail.com>
2022-03-17 01:09:40 -0300

    moficacion en subtitulo

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -80,7 +80,7 @@ const Formulario = ({pacientes, setPacientes,paciente, setPaciente}) => {
     //mx separa en horizontal
     <div className="md:w-1/2 lg:w-2/5">
         <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
-        <p className="text-lg mt-5 text-center mb-10">
+        <p className="text-xl mt-5 text-center mb-10">
           Añade Pacientes y {''}
           <span className="text-indigo-600 font-bold">Administralos</span>
         </p>
